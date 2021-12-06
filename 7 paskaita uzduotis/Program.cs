d8ed6dc1c1f9455e6d5e32f3c3c88aa7fe0abb29
GustasGH <gustas.brazenas@gmail.com>
2021-12-06 20:10:19 +0200

    Istrinti komentarai

--- 7 paskaita uzduotis/Program.cs
+++ 7 paskaita uzduotis/Program.cs
@@ -38,9 +38,4 @@ namespace _7_paskaita_uzduotis
     }
 }
 
-// 1 tri int
-//2 trimestro pazymis
-//3 trimestro pazymis
-//Int ( sudeti visus ir padalinti is 3)
-//    naudoti struct (jame parasyti metoda kuris isveta metini vidurki, struc viduj deklaruoti kintamuosiu ir parasyt metoda kuris isveda metiniu pazymiu vidurki)
 
